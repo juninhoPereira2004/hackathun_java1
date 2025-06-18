6d9b8a384f42f4a919996b56989f6b5f6150852b
Seu Nome <seuemail@example.com>
2025-06-18 20:19:29 -0300

    iniciando projeto

---
+++ src/main/java/hackathun_java1/hackathun/HackathunApplication.java
@@ -0,0 +1,13 @@
+package hackathun_java1.hackathun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathun_java1/hackathun/HackathunApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathun_java1.hackathun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
